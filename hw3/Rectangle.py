44f917e65660181d8c9edbc1b300e3fa015715d0
root <root@ip-172-31-19-127.us-east-2.compute.internal>
2026-09-10 21:11:52 +0000

    finish rectangle problem

--- hw3/Rectangle.py
+++ hw3/Rectangle.py
@@ -1,4 +1,12 @@
-class Rectangle: 
-    length = 0
-    width = 0
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+        
+    def calc_area(self):
+        return self.length * self.width
 
+
+rectangle1 = Rectangle(5, 3)
+
+print("Area of the rectangle:", rectangle1.calc_area())
