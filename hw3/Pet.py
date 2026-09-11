c9b881775a8c6670680afe260e88c2021539e5fb
Alex Gnehm <a02244896@usu.edu>
2026-09-11 13:10:18 -0600

    minor adjustments to clean up code and add comments.

--- hw3/Pet.py
+++ hw3/Pet.py
@@ -1,7 +1,5 @@
-class Pet: 
-    species = "dog" #store the species of the pet
-
-    def __init__(self, name, age, species): 
+class Pet: #Pet class
+    def __init__(self, name, age, species): #initialize the pet with name, age, and species
         self.name = name 
         self.age = age
         self.species = species
@@ -9,7 +7,7 @@ class Pet:
     def human_years_calc(self):
         return self.age * 7
 
-    def average_lifespan(self, species):
+    def average_lifespan(self, species): #grab the average life span of the pet based on species
         if species == "dog": 
             return 13
         elif species == "cat":
@@ -20,11 +18,11 @@ class Pet:
             return "Unknown species"
 
 
-pet_1 = Pet("Stitch", 13, "dog")
+pet_1 = Pet("Stitch", 13, "dog") #pet object, with name, age, and species
 print("Pet 1",pet_1.name,"is",pet_1.human_years_calc(),"in human years. with a average lifespan of ",pet_1.average_lifespan(pet_1.species)," years.")
 
-pet_2 = Pet("Lucy", 5, "cat")
+pet_2 = Pet("Lucy", 5, "cat")#pet object, with name, age, and species
 print("Pet 2",pet_2.name,"is",pet_2.human_years_calc(),"in human years. with a average lifespan of ",pet_2.average_lifespan(pet_2.species)," years.")
 
-pet_3 = Pet("Max", 2, "bird")
+pet_3 = Pet("Max", 2, "bird")#pet object, with name, age, and species
 print("Pet 3",pet_3.name,"is",pet_3.human_years_calc(),"in human years. with a average lifespan of ",pet_3.average_lifespan(pet_3.species)," years.")
