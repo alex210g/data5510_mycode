ab3c0eaf859cff15063f5c5eecb6d999894839bc
root <root@ip-172-31-10-125.us-west-1.compute.internal>
2026-09-23 16:57:47 +0000

    worked example for pulling json

--- class_notes/notes_09-21/web_json_api_duck_example.py
+++ class_notes/notes_09-21/web_json_api_duck_example.py
@@ -4,21 +4,20 @@ The associated words are given a score.
 '''
 
 #easy 4 step method for getting data from a web data json
-# 1. put it into a browswer and look at it 
-
-# 3. see what you actually need
-# 4. write the code to request the data
+# 1. put it into a browswer 
+# figure out how the data works
+# 3. see what you actually need, get the keys.
+# 4. write the code to request the data and process the data
 
 
 import requests # needs to be pip installed
 import json # should be native
 
-# example url to query datamuse web json api
-example_url = "https://api.datamuse.com/words?ml=duck"
+
 
 # variables to query alphavantage
-word = 'duck'
-search_word = "dunk"
+word = 'aggies'
+search_word = "usu"
 
 #make it obvious that it is a key by using the word key
 key_word = "word"
@@ -28,60 +27,22 @@ key_score = "score"
 url = 'https://api.datamuse.com/words?ml=' + word
 print(url)
 
+
+
+
 #these are native libraries and are imported above
 # requests stock data from data muse, web request object
 request = requests.get(url) #pulls the url stored above. This does the same thing as entering the url into a browser
+print(request.text)
+
+
 # print(request.text) # print to double check data from web json api is good
 dct_full = json.loads(request.text) # the s stands for string in the loads word, turns all the text and turns it into python data for you. Which is stored in dct_full
+print(dct_full)
+
+for dct in dct_full: 
+    if dct["word"] == search_word:
+        val = dct["score"]
 
 
-
-#####################################################################
-# programming activity
-# What is the score of the associated word: mallard
-# Steps: 
-# 1. load json into a dictionary
-# 2. search for word "mallard"
-# 3. print associated score value for mallard
-
-# answer below, try yourself before looking
-
-
-# dunk
-
-for word_2 in dct_full:
-    if word_2[key_word] == search_word:
-        print(word_2[key_score])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# for dct_small in dct_full:
-#     # print(dct_small) # print all values to verify data is good
-#     if dct_small[key_word] == search_word:
-#         print("word: ", dct_small[key_word])
-#         print("value: ", dct_small[key_score])
-        
+print(val)
